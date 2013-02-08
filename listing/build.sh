3e3199b3b1565c45e67681a646c1a24502b9d383
Mahemoff <michael@mahemoff.com>
2013-02-08 16:45:30 +1100

    reformat build (no blank lines needed)

--- listing/build.sh
+++ listing/build.sh
@@ -3,9 +3,6 @@
 JS=build/series/widget.js
 haml src/host.haml build/host.html
 stylus < src/series/index.stylus > build/series/index.css
-echo '' > $JS
 tplcpl -t src/series/templates -o $JS
-echo '' >> $JS
 cat lib/jquery.js lib/jquery.jplayer.js lib/jplayer.playlist.js >> $JS
-echo '' >> $JS
 coffee -c -p src/series/index.coffee >> $JS
