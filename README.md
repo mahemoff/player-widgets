1078e5fec51e5541cc034275b90a561a377a5af7
Nino Paolo Amarillento <o70vd@yahoo.com>
2013-03-25 03:33:41 +0800

    Deleted old files and replaced with flexible player

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-player-widgets
-==============
+# Flexible jPlayer Skin
--- listing/run.sh
+++
@@ -1,4 +0,0 @@
-cd `dirname $0`/build
-#/usr/bin/open -a "/Applications/Google Chrome.app" "http://localhost:8000"
-echo 'Widget test will appear at http://localhost:8000/host.html'
-python -m SimpleHTTPServer
